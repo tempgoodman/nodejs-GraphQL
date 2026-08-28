64570da834202df7af0cb8a720036bcf759bc9ec
pong.ng@dreamshub.com <pong.ng@dreamshub.com>
2026-08-28 09:13:59 +0800

    fix: stabilize microservices orchestration and docker networking

--- xapi-gateway/start.sh
+++ xapi-gateway/start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
+set -e
 
-echo "wait 5 seconds ..."
-sleep 5 
+sleep 20
 
-echo "start dynamic composition of Supergraph"
 export APOLLO_ELV2_LICENSE="accept"
-npx @apollo/rover supergraph compose --config ./supergraph-prod.yaml --output supergraph.graphql
 
-echo "Supergraph composition completed, ready to start the Gateway!"
+rover supergraph compose --config ./supergraph-prod.yaml --output ./supergraph.graphql
+
 npm run start
